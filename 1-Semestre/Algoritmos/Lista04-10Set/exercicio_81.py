28ec6f9f146844a1aa0e657fe2a56ddf5fdbc649
Elbert Jean <elbertjeandossantos@gmail.com>
2023-09-09 12:31:52 -0300

    exercicio 81

--- 1-Semestre/Algoritmos/Lista04-10Set/exercicio_81.py
+++ 1-Semestre/Algoritmos/Lista04-10Set/exercicio_81.py
@@ -1,12 +1,12 @@
 conta = int(input('\nDigite a conta de três dígitos: '))
-d1 = float(conta / 100)
-d2 = float(conta % 100 // 100)
-d3 = float(conta % 100 % 10)
-inv = float(d3 * 100 + d2 * 10 + d1)
-soma = float(conta + inv)
-d1 = (soma / 100) * 1
-d2 = (soma % 100 / 10) * 2
-d3 = (soma % 100 % 10) * 3
-digito = (d1 + d2 + d3) % 10
-print(f'\nDigito verificador: {digito:.3f}')
+d1 = int(conta / 100)
+d2 = int(conta % 100 / 10)
+d3 = int(conta % 100 % 10)
+inv = int(d3 * 100 + d2 * 10 + d1)
+soma = int(conta + inv)
+d1 = int(soma / 100) * 1
+d2 = int(soma % 100 / 10) * 2
+d3 = int(soma % 100 % 10) * 3
+digito = int(d1 + d2 + d3) % 10
+print(f'\nDigito verificador: {digito}')
 print('\n')
